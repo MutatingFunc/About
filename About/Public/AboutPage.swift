88343dbc1f7738c1829c993d68d55f895d0f8ea8
James Froggatt <james.froggatt@me.com>
2025-08-28 11:01:46 +0100

    iOS 26 specialisations

--- About/Public/AboutPage.swift
+++ About/Public/AboutPage.swift
@@ -18,126 +18,162 @@ public struct AboutPage: View {
     @Environment(\.dynamicTypeSize) private var dynamicTypeSize
     
     public var body: some View {
-        ScrollViewReader { scrollView in
-            ScrollView {
-                VStack(alignment: .leading) {
-                    if !config.features.isEmpty {
-                        let unlockHeadingText = Text("Unlock Features")
-                        let unlockHeading = (Text("🔑 ") + unlockHeadingText)
-                            .accessibilityLabel(unlockHeadingText)
-                            .font(.headline)
-                            .accessibilityHeading(.h2)
-                            .accessibilityAddTraits(.isHeader)
-                        ViewThatFits(in: .horizontal) {
-                            HStack(alignment: .firstTextBaseline) {
-                                unlockHeading
-                                    .lineLimit(1)
-                                RestorePurchasesButton(iapCount: config.features.count)
-                                    .lineLimit(1)
-                                    .onAppear { inlineRestore = true }
-                                    .onDisappear { inlineRestore = false }
-                            }
-                            unlockHeading
-                        }
-                        Text("I work on this app independently. Your support means a lot!")
-                            .font(.footnote)
-                        if !inlineRestore {
-                            RestorePurchasesButton(iapCount: config.features.count)
-                        }
-                        IAPView(iaps: config.features)
-                            .multilineTextAlignment(.center)
-                            .padding(.vertical, 4)
-                        if config.features.reduce(true, { $0 && $1.isKnownUnlocked }) {
-                            Text("You unlocked the full app. Thank you so much! 🥰")
-                                .font(.footnote.weight(.medium))
-                                .padding(.top, 2)
-                        }
-                        Divider()
-                            .padding(.vertical, 8)
-                    }
-                    let myAppsHeadingText = Text("My Apps")
-                    (Text("✨ ") + myAppsHeadingText)
-                        .accessibilityLabel(myAppsHeadingText)
-                        .font(.headline)
-                        .accessibilityHeading(.h2)
-                        .accessibilityAddTraits(.isHeader)
-                    Text("If you're enjoying \(config.app.name), you might like my other works too!")
-                        .font(.footnote)
-                    MyAppsView(currentApp: config.app)
-                        .multilineTextAlignment(.center)
-                        .padding(.vertical, 4)
-                    if !config.tips.isEmpty {
-                        Divider()
-                            .padding(.vertical, 8)
-                        let tipJarHeadingText = Text("Help Out")
-                        (Text("💝 ") + tipJarHeadingText)
-                            .accessibilityLabel(tipJarHeadingText)
-                            .font(.headline)
-                            .accessibilityHeading(.h2)
-                            .accessibilityAddTraits(.isHeader)
-                        Text("If you're feeling generous, you can directly support me in bringing you new features regularly!")
-                            .font(.footnote)
-                        Button {
-                            requestReview()
-                        } label: {
-                            let layout = dynamicTypeSize.isAccessibilitySize ? AnyLayout(VStackLayout(alignment: .leading)) : AnyLayout(HStackLayout())
-                            layout {
-                                Image(systemName: "message")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(maxHeight: 48)
-                                    .padding(.horizontal, 4)
-                                    .accessibilityHidden(true)
-                                    .foregroundStyle(Color.accentColor.gradient)
-                                VStack(alignment: .leading) {
-                                    Text("Leave A Review")
-                                        .font(.callout)
-                                        .padding(.bottom, 1)
-                                    Text("This helps the app get seen")
-                                        .font(.caption)
-                                        .foregroundStyle(.secondary)
+        NavigationStack {
+            ScrollViewReader { scrollView in
+                let scrollView = ScrollView {
+                    VStack(alignment: .leading) {
+                        if !config.features.isEmpty {
+                            let unlockHeadingText = Text("Unlock Features")
+                            let unlockHeading = (Text("🔑 ") + unlockHeadingText)
+                                .accessibilityLabel(unlockHeadingText)
+                                .font(.headline)
+                                .accessibilityHeading(.h2)
+                                .accessibilityAddTraits(.isHeader)
+                            ViewThatFits(in: .horizontal) {
+                                HStack(alignment: .firstTextBaseline) {
+                                    unlockHeading
+                                        .lineLimit(1)
+                                    RestorePurchasesButton(iapCount: config.features.count)
+                                        .lineLimit(1)
+                                        .onAppear { inlineRestore = true }
+                                        .onDisappear { inlineRestore = false }
                                 }
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .multilineTextAlignment(.leading)
-                                .foregroundStyle(.primary)
+                                unlockHeading
                             }
-                            .padding()
-                            .frame(maxWidth: .infinity)
-                            .about_iap()
+                            Text("I work on this app independently. Your support means a lot!")
+                                .font(.footnote)
+                            if !inlineRestore {
+                                RestorePurchasesButton(iapCount: config.features.count)
+                            }
+                            IAPView(iaps: config.features)
+                                .multilineTextAlignment(.center)
+                                .padding(.vertical, 4)
+                            if config.features.reduce(true, { $0 && $1.isKnownUnlocked }) {
+                                Text("You unlocked the full app. Thank you so much! 🥰")
+                                    .font(.footnote.weight(.medium))
+                                    .padding(.top, 2)
+                            }
+                            Divider()
+                                .padding(.vertical, 8)
                         }
-                        .buttonStyle(.plain)
-                        .padding(.vertical, 4)
-                        IAPView(iaps: config.tips)
+                        let myAppsHeadingText = Text("My Apps")
+                        (Text("✨ ") + myAppsHeadingText)
+                            .accessibilityLabel(myAppsHeadingText)
+                            .font(.headline)
+                            .accessibilityHeading(.h2)
+                            .accessibilityAddTraits(.isHeader)
+                        Text("If you're enjoying \(config.app.name), you might like my other works too!")
+                            .font(.footnote)
+                        MyAppsView(currentApp: config.app)
                             .multilineTextAlignment(.center)
                             .padding(.vertical, 4)
-                    }
-                }
-                .multilineTextAlignment(.leading)
-                .safeAreaPadding()
-                .background {
-                    GeometryReader { geometry in
-                        Color.clear.onChange(of: -geometry.frame(in: .scrollView(axis: .vertical)).minY, initial: true) { oldValue, newValue in
-                            let new = if newValue > oldValue {
-                                newValue < 16
-                            } else {
-                                newValue < 24
+                        if !config.tips.isEmpty {
+                            Divider()
+                                .padding(.vertical, 8)
+                            let tipJarHeadingText = Text("Help Out")
+                            (Text("💝 ") + tipJarHeadingText)
+                                .accessibilityLabel(tipJarHeadingText)
+                                .font(.headline)
+                                .accessibilityHeading(.h2)
+                                .accessibilityAddTraits(.isHeader)
+                            Text("If you're feeling generous, you can directly support me in bringing you new features regularly!")
+                                .font(.footnote)
+                            Button {
+                                requestReview()
+                            } label: {
+                                let layout = dynamicTypeSize.isAccessibilitySize ? AnyLayout(VStackLayout(alignment: .leading)) : AnyLayout(HStackLayout())
+                                layout {
+                                    Image(systemName: "message")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(maxHeight: 48)
+                                        .padding(.horizontal, 4)
+                                        .accessibilityHidden(true)
+                                        .foregroundStyle(Color.accentColor.gradient)
+                                    VStack(alignment: .leading) {
+                                        Text("Leave A Review")
+                                            .font(.callout)
+                                            .padding(.bottom, 1)
+                                        Text("This helps the app get seen")
+                                            .font(.caption)
+                                            .foregroundStyle(.secondary)
+                                    }
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .multilineTextAlignment(.leading)
+                                    .foregroundStyle(.primary)
+                                }
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .about_iap()
                             }
-                            if new != self.isExpanded {
-                                self.isExpanded = new
+                            .buttonStyle(.plain)
+                            .padding(.vertical, 4)
+                            IAPView(iaps: config.tips)
+                                .multilineTextAlignment(.center)
+                                .padding(.vertical, 4)
+                        }
+                    }
+                    .multilineTextAlignment(.leading)
+                    .safeAreaPadding()
+                    .background {
+                        GeometryReader { geometry in
+                            Color.clear.onChange(of: -geometry.frame(in: .scrollView(axis: .vertical)).minY, initial: true) { oldValue, newValue in
+                                let new: Bool
+                                if #available(iOS 26, *) {
+                                    new = newValue < 8
+                                } else {
+                                    new = if newValue > oldValue {
+                                        newValue < 16
+                                    } else {
+                                        newValue < 24
+                                    }
+                                }
+                                if new != self.isExpanded {
+                                    self.isExpanded = new
+                                }
                             }
                         }
                     }
+                    .presentationBackground(.background.secondary)
                 }
-                .presentationBackground(.background.secondary)
+                    .productViewStyle(.compact)
+                    .navigationBarBackButtonHidden()
+                if #available(iOS 26, *) {
+                    scrollView
+                        .toolbar {
+                            ToolbarItem(placement: .topBarLeading) {
+                                Button(role: .close) {
+                                    dismiss()
+                                }
+                            }
+                            ToolbarItem(placement: .topBarLeading) {
+                                header(expanded: false)
+                                    .compositingGroup()
+                                    .opacity(isExpanded ? 0 : 1)
+                                    .accessibilityHidden(true)
+                                    .animation(.default.speed(2), value: isExpanded)
+                            }.sharedBackgroundVisibility(.hidden)
+                            ToolbarItem(placement: .title) {
+                                Color.clear
+                            }
+                            ToolbarItem(placement: .largeTitle) {
+                                header(expanded: true)
+                                    .compositingGroup()
+                                    .opacity(isExpanded ? 1 : 0)
+                                    .animation(.default.speed(2), value: isExpanded)
+                            }.sharedBackgroundVisibility(.hidden)
+                        }
+                } else {
+                    scrollView
+                        .safeAreaInset(edge: .top, spacing: 0) {
+                            header(expanded: true).hidden()
+                        }
+                        .overlay(alignment: .top) {
+                            header(expanded: isExpanded)
+                                .animation(.default.speed(2), value: isExpanded)
+                        }
+                }
             }
-            .safeAreaInset(edge: .top, spacing: 0) {
-                header(expanded: true).hidden()
-            }
-            .overlay(alignment: .top) {
-                header(expanded: isExpanded)
-                    .animation(.default.speed(2), value: isExpanded)
-            }
-            .productViewStyle(.compact)
         }
     }
     
@@ -146,32 +182,16 @@ public struct AboutPage: View {
     @ViewBuilder
     func header(expanded: Bool) -> some View {
         VStack(spacing: 0) {
-            let dismissButton = Button {
-                dismiss()
-            } label: {
-                Text("Done")
-                    .fontWeight(.medium)
-                    .padding(8)
-                    .hoverEffect(.highlight)
-                    .frame(minWidth: 44, minHeight: 44)
-            }
             let title = Group {
-                Text(config.app.name).font(.largeTitle).bold()
+                Title(appName: config.app.name)
                     .accessibilityHidden(true) // Use button as heading
                 if expanded {
-                    Link(destination: URL(string: "https://mutatingfunc.github.io/")!) {
-                        let byLine = "by Jamie"
-                        (Text("\(byLine) ") + Text("\(Image(systemName: "arrow.up.right.square"))").font(.callout))
-                            .accessibilityLabel(byLine).accessibilityHint(Text("Opens my blog, which includes contact info"))
-                            .font(.title3)
-                            .opacity(isHoveringBlogLink ? 0.6 : 1)
-                            .animation(.default, value: isHoveringBlogLink)
-                    }.onHover { isHovering in
-                        isHoveringBlogLink = isHovering
-                    }
-                    .transition(.blurReplace.combined(with: .move(edge: .top)))
+                    Subtitle()
+                        .buttonStyle(.borderless)
+                        .foregroundStyle(.tint)
+                        .transition(.blurReplace.combined(with: .move(edge: .top)))
                 }
-            }.lineLimit(nil)
+            }.lineLimit(nil).fixedSize()
             HStack {
                 let iconHeight: Double = expanded ? 76 : 48
                 Color.clear
@@ -186,15 +206,60 @@ public struct AboutPage: View {
                 VStack(alignment: .leading) {
                     title
                 }.frame(maxWidth: .infinity, alignment: .leading)
-                dismissButton
+                if #available(iOS 26, *) {} else {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text("Done")
+                            .fontWeight(.medium)
+                            .padding(8)
+                            .hoverEffect(.highlight)
+                            .frame(minWidth: 44, minHeight: 44)
+                    }
+                }
             }
             .multilineTextAlignment(.leading)
             .safeAreaPadding([.top, .horizontal], expanded ? 8 : 4)
             .padding(.bottom, expanded ? 8 : 4)
             
-            Divider()
+            if #available(iOS 26, *) {
+                Divider()
+                    .padding(.horizontal, -100)
+                    .opacity(expanded ? 1 : 0)
+            } else {
+                Divider()
+                    .padding(.horizontal, -100)
+            }
         }
-        .background(.background.secondary)
+        .background {
+            if #available(iOS 26, *) {} else {
+                Rectangle()
+                    .fill(.background.secondary)
+            }
+        }
+    }
+}
+
+struct Title: View {
+    var appName: String
+    var body: some View {
+        Text(appName).font(.largeTitle).bold()
+    }
+}
+struct Subtitle: View {
+    @State private var isHoveringBlogLink = false
+    
+    var body: some View {
+        Link(destination: URL(string: "https://mutatingfunc.github.io/")!) {
+            let byLine = "by Jamie"
+            (Text("\(byLine) ") + Text("\(Image(systemName: "arrow.up.right.square"))").font(.callout))
+                .accessibilityLabel(byLine).accessibilityHint(Text("Opens my blog, which includes contact info"))
+                .font(.title3)
+                .opacity(isHoveringBlogLink ? 0.6 : 1)
+                .animation(.default, value: isHoveringBlogLink)
+        }.onHover { isHovering in
+            isHoveringBlogLink = isHovering
+        }
     }
 }
 
